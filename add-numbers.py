fb1cd41ee4c990a8f7382d4141a99a2f52cd484d
adrialarmijo <adrial.armijo@gmail.com>
2021-12-13 18:01:13 -0800

    v1

--- add-numbers.py
+++ add-numbers.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+num1 = input("Hello! Please input your first number: ")
+num2 = input("Thank you, please enter your second number: ")
+add = float(num1) + float(num2)
+subtract = float(num1) - float(num2)
+print("Your addition output is " + str(add))
+print("Your subtraction output is: " + str(subtract))
