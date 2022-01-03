cc7adeeee970b395684cb8042d905d3a1ede8343
adrialarmijo <adrial.armijo@gmail.com>
2022-01-02 18:36:53 -0800

    updated with cleaner code

--- sum-of-arrays/sum-of-arrays.py
+++ sum-of-arrays/sum-of-arrays.py
@@ -1,8 +1,10 @@
 
-sum = 0
+sumOf = 0
+i = 0
 array1 = []
 array2 = []
 array3 = []
+sumArray = []
 
 print("Welcome to the Array Summizer!")
 print("How this works: Enter any number of numbers into three different arrays and this program will count their sums!")
@@ -10,30 +12,19 @@ print("For more fun, let's play a guessing game!")
 guess = input("What do you think the sum will be? ")
 
 array1 = [float(item) for item in input("Please enter items for the first array: ").split()]
+sumArray.append(array1)
 array2 = [float(item) for item in input("Please enter items for the second array: ").split()]
+sumArray.append(array2)
 array3 = [float(item) for item in input("Please enter items for the third array: ").split()]
+sumArray.append(array3)
 
-#Find the length of the longest array
-longestArrayLength = max(map(len, (array1, array2, array3)))
+print("Adding the following numbers:")
+print(" " + str(array1))
+print(" " + str(array2))
+print("+" + str(array3))
+print("--------------------------")
+sumOf = sum(map(sum, sumArray))
 
-i = 0
-totalSum = 0
-while i < longestArrayLength:
-    if(array1[i] is None):
-        sum += 0
-    if(array2[i] is None):
-        sum += 0
-    if(array3[i] is None):
-        sum += 0
-    else:
-        sum = array1[i] + array2[i] + array3[i]
-        print("\tAdding: " + str(array1[i]) + " + " + str(array2[i]) + " + " + str(array3[i]))
-
-    totalSum += sum
-    print("The total so far is: " + str(totalSum))
-    i += 1
-
-answer = float(totalSum) is float(guess)
-print(str(totalSum) + " " + str(guess))
-print("The final total is: " + str(totalSum))
+answer = float(sumOf) == float(guess)
+print("The sum is: " + str(sumOf))
 print("Your guess was: " + str(answer))
