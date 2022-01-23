dab80156197feeb1a854ee2543740a8f3fc83f7f
adrialarmijo <adrial.armijo@gmail.com>
2022-01-22 16:32:36 -0800

    removed redunancies

--- mixing-colors/ColorSwitch.py
+++ mixing-colors/ColorSwitch.py
@@ -1,19 +1,15 @@
 class ColorSwitch:
     def __init__(self):
-        self.primaryColors = {1: 'red', 
-                              2: 'blue',
-                              4: 'yellow'}
+        self.primaryColors = {'red'     : 1, 
+                              'blue'    : 2,
+                              'yellow'  : 4}
 
-        self.mixedColors = {  3: 'purple',
-                              5: 'orange',
-                              6: 'green'}
+        self.mixedColors = {  3 : 'purple',
+                              5 : 'orange',
+                              6 : 'green'}
 
     def combine(self, colorSelected_1, colorSelected_2) -> str:
         if(colorSelected_1 == colorSelected_2):
             return colorSelected_1
-        for key_1, color_1 in self.primaryColors.items():
-            if colorSelected_1 == color_1:
-                for key_2, color_2 in self.primaryColors.items():
-                    if colorSelected_2 == color_2:
-                        k = key_2 + key_1
-                        return self.mixedColors[k]
+        k = self.primaryColors[colorSelected_1] + self.primaryColors[colorSelected_2]
+        return self.mixedColors[k]
